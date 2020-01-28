29254270748746fdbfaa0fd0589b6ea52654fa94
Thomas Greenway <thomas.greenway@g.austincc.edu>
2020-01-27 19:07:56 -0600

    Void and Return functions with a test for them.

--- src/examples/01_module/02_variables/main.cpp
+++ src/examples/01_module/02_variables/main.cpp
@@ -1,6 +1,14 @@
+#include "vars.h"
 
 int main() 
 {
+	display_number(10);
+	display_number(20);
+	
+	int num = 5;
 
+	cout << square_number(num)<<"\n";
+	cout << num;
+	int num1 = square_number(5);
 	return 0;
 }
--- src/examples/01_module/02_variables/vars.cpp
+++ src/examples/01_module/02_variables/vars.cpp
@@ -1,1 +1,11 @@
+#include "vars.h"
 //cpp
+void display_number(int num1)
+{
+	cout << num1 << "\n";
+}
+
+int square_number(int num)
+{
+	return num * num;
+}
