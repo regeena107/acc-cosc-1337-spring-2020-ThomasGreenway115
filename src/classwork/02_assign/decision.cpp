0b4bc339631cccc9c6a7518d6869f76b1d0d4a99
9419302 <9419302@HLC2411-02>
2020-02-05 19:22:45 -0600

    FIxed test not working.

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -7,11 +7,11 @@ double gross_pay(double hours, double hourly_rate)
 	double pay = 0;
 	if (hours > 40)
 	{
-		double pay = (40 * hourly_rate) + (hours - 40) * (hourly_rate * 1.5);
+		pay = (40 * hourly_rate) + (hours - 40) * (hourly_rate * 1.5);
 	}
 	else
 	{
-		double pay = (hours * hourly_rate);
+		pay = (hours * hourly_rate);
 	}
 	return pay;
 }
