b28d5c948ac519d31854d5df92463838c2653d2a
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-12 22:00:51 -0600

    cw 2 grading: main incomplete

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,15 +1,31 @@
-//write includes statements
-#include"loops.h"
+#include "loops.h"
+#include<iostream>
+
+using std::cout; using std::cin;
+
 //write using statements for cin and cout
 
 
 /*
-Use a do while loop to prompt the user for 
+Use a do while loop to prompt the user for
 a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
-int main() 
+int main()
 {
-	factorial;
+	int num, choice;
+
+	do
+	{
+		cout << "Enter number: ";
+		cin >> num;
+
+		int sum = factorial(num);
+		cout << "Number is: " << sum << "\n";
+		cout << "Enter 1 to continue...";
+		cin >> choice;
+
+	} while (choice == 1);
+
 	return 0;
 }
