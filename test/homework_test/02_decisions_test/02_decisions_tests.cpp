6fbebe7ce8c7070f6f172704b6400ad1d708848d
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-14 23:01:15 -0600

    hw 2 grading: add assertion

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -17,4 +17,5 @@ TEST_CASE("Test to calculate gpa")
 	REQUIRE(calculate_gpa(12, 45) == 3.75);
 	REQUIRE(calculate_gpa(120, 390) == 3.25);
 	REQUIRE(calculate_gpa(90, 180) == 2.00);
+	REQUIRE(calculate_gpa(9, 0) == -1);
 }
