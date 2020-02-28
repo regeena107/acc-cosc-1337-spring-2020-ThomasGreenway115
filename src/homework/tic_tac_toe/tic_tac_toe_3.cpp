94ba34e7f2f41022e97494908d8dba9ac39f77ce
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2020-02-28 11:42:24 -0800

    Add files via upload

--- src/homework/tic_tac_toe/tic_tac_toe_3.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_3.cpp
@@ -28,21 +28,4 @@ Win diagonally
 0 1 2
 3 4 5
 6 7 8
-
 */
-bool TicTacToe3::check_diagonal_win()
-{
-	if (pegs[0] == pegs[4] && pegs[4] == pegs[8] && pegs[8] != " ")
-	{
-		return true;
-	}
-	else if (pegs[2] == pegs[4] && pegs[4] == pegs[6] && pegs[6] != " ")
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
