589c6d430fa2fae7c503ddca85ed4cfbb820c75e
Thomas Greenway <TheTman234@gmail.com>
2020-03-01 13:21:17 -0600

    Started Homework 5

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,31 @@
 //h
+#include <iostream>
+using std::string;
+class error
+{
+public:
+	error(string msg) : message{ msg } {};
+	
+	string get_message() { return message; };
+
+
+private:
+	string message;
+
+};
+class TicTacToe
+{
+public:
+	void start_game(string first_player);
+	
+	void mark_board(int postion);
+
+	string get_player()const;
+private:
+	void set_next_player();
+
+	string player;
+
+
+
+};
